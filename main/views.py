aee18bc788b57d7b600f9701126c389c4525a003
JonasSousa <jonassousa.dev@gmail.com>
2023-09-12 21:57:37 +0100

    login + logout

--- main/views.py
+++ main/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm  # Importe o formulário de registro apropriado
 
-# Create your views here.
-
+@login_required(login_url='/login')
 def home(request):
-    return render(request,'main/home.html')
+    return render(request, 'main/home.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Salve o usuário
+            login(request, user)  # Faça login automaticamente após o registro
+            return redirect('home')  # Redirecione para a página inicial ou para onde você desejar
+
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/sign_up.html', {'form': form})
--- website/settings.py
+++ website/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5'
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -80,6 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'USER':'',
+        # 'PASSWORD':'',
+        # 'HOST':'',
+        # 'PORT':'',
     }
 }
 
@@ -124,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/login'

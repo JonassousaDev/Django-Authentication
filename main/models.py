3481f73deaf78f7ce9d13a28dd8252d3f04b9fd3
JonasSousa <jonassousa.dev@gmail.com>
2023-09-12 22:22:10 +0100

    post form

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Relacionamento com o autor (supondo que você está usando o modelo de usuário padrão do Django)
+    title = models.CharField(max_length=255)  # Título do post
+    description = models.TextField()  # Descrição do post
+    created_at = models.DateTimeField(auto_now_add=True)  # Data e hora de criação do post (será preenchida automaticamente na criação)
+    updated_at = models.DateTimeField(auto_now=True)  # Data e hora da última atualização do post (será atualizada automaticamente na atualização)
+
+    def __str__(self):
+        return self.title  + '\n' + self.description # Representação em string do post (usando o título)
--- main/views.py
+++ main/views.py
@@ -1,11 +1,29 @@
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import RegisterForm  # Importe o formulário de registro apropriado
+# Importe o formulário de registro apropriado
+from .forms import RegisterForm, PostForm
 
 @login_required(login_url='/login')
 def home(request):
     return render(request, 'main/home.html')
+
+@login_required(login_url='/login')
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user  # Defina o autor do post como o usuário autenticado
+            post.save()
+            return redirect('home')  # Redirecione para a página inicial ou outra página apropriada após a criação do post
+
+    else:
+        form = PostForm()
+
+    return render(request, 'main/create_post.html', {'form': form})
+
+
 
 def sign_up(request):
     if request.method == 'POST':
